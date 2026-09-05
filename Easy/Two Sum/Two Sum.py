baf683de5124db0660b79660c6b937982c8d7b57
ANISH AGARWAL <agarwalanish270902@gmail.com>
2026-09-06 02:28:50 +0530

    Add solution for Two Sum (Easy)

--- Easy/Two Sum/Two Sum.py
+++ Easy/Two Sum/Two Sum.py
@@ -1,13 +1,9 @@
 /*
 Problem Name: Two Sum
 Difficulty: Easy
-URL: https://leetcode.com/problems/two-sum/submissions/
+URL: https://leetcode.com/problems/two-sum/submissions/1955545117/
 Submitted Date: 2026-09-05
 Repository: Anni27-hub/Leetcode_DSA
 */
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-    }
-}
+c
